6ff7b59d28fcbf68b48a87abb20df951e492ff9f
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-12-15 22:57:20 +0100

    cleanup

--- src/components/radio-button/favorites.js
+++ src/components/radio-button/favorites.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { StationButton } from './station-button';
 import styled from 'styled-components';
 import { SearchPanelToggleButton, SearchPanel } from '../search/search-panel';
