3a09ed45838397bae169da250b713b8eb7c880da
lkwg82 <lkwg82@users.noreply.github.com>
2021-06-16 11:40:23 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.4435d2e566776e63f4c8c27173501fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c540b1ae5910975735233dddc3d846",
+    "url": "/webradio/index.html"
+  },
+  {
+    "revision": "9a2981d53a9d36731f80",
+    "url": "/webradio/static/css/2.f2e2993a.chunk.css"
+  },
+  {
+    "revision": "b4e2b42c4b6662581ab2",
+    "url": "/webradio/static/css/main.3cf1caaf.chunk.css"
+  },
+  {
+    "revision": "9a2981d53a9d36731f80",
+    "url": "/webradio/static/js/2.e888b234.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/webradio/static/js/2.e888b234.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4e2b42c4b6662581ab2",
+    "url": "/webradio/static/js/main.b1d1472a.chunk.js"
+  },
+  {
+    "revision": "ae7dcc841ddef6f57bd6",
+    "url": "/webradio/static/js/runtime-main.9efcb38f.js"
+  }
+]);
